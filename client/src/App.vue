a8561c97ea0e3a8a19f39f49812f7f7cfa0aee0d
Logan Garay <garay.logan@gmail.com>
2020-01-14 15:46:40 -0700

    set up files and got login page going

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div id="nav" v-show="login">
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      login: false
+    };
+  },
+  mounted() {
+    if (this.$route.name == "login") {
+      this.login = false;
+    } else if (this.$route.name !== "login") {
+      this.login = true;
+    }
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
