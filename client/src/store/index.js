e61398ef27c850afe2c68002f93ad418ce2910e5
Logan Garay <garay.logan@gmail.com>
2020-01-14 16:38:47 -0700

    can create user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import UserService from "../UserService";
+import router from "../router/index";
 
 Vue.use(Vuex);
 
@@ -16,7 +18,42 @@ export default new Vuex.Store({
     comments: [],
     photos: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    }
+  },
+  actions: {
+    //#region -- AUTH STUFF --
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await UserService.Register(creds);
+        commit("setUser", user);
+        router.push({ name: "boards" });
+      } catch (e) {
+        console.warn(e.message);
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await UserService.Login(creds);
+        commit("setUser", user);
+        router.push({ name: "boards" });
+      } catch (e) {
+        console.warn(e.message);
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+        let success = await UserService.Logout();
+        if (!success) {
+        }
+        commit("resetState");
+        router.push({ name: "login" });
+      } catch (e) {
+        console.warn(e.message);
+      }
+    }
+  },
   modules: {}
 });
