4753a98bfba331ea506516342475f00fa2db9ae7
DevynWright <Wrightdevyn@gmail.com>
2020-01-15 10:25:24 -0700

    navbar in profile view

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,7 +1,18 @@
-<template></template>
+<template>
+
+<div class="container-fluid" :style="{ 'background-image': 'url(https://scottcoyneunderdev.files.wordpress.com/2015/02/dark-wood-high-quality-wallpaper-hd-resolution3.jpg)' }">
+<div class="row"><navbar /></div>
+
+</div>
+</template>
 
 <script>
-export default {};
+import Navbar from "@/components/NavBar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
 </script>
 
 <style>
