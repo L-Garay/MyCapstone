bb19fb5e48d9eecc66da569360bb756b93e55247
DevynWright <Wrightdevyn@gmail.com>
2020-01-15 11:41:53 -0700

    routes are set

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
+import Create from "../views/Create.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +18,21 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile
+  },
+  {
+    path: "/create",
+    name: "create",
+    component: Create
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About
   }
 ];
 
