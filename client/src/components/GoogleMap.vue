b30b0e299410c3482cdf5a25bf2d6de39866896a
EricRooney <erooneous@gmail.com>
2020-01-16 15:47:24 -0700

    Map Component Works

---
+++ client/src/components/GoogleMap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="googleMap" style="width: 70vw; height: 300px"></div>
+</template>
+<script>
+export default {
+  name: "GoogleMap",
+  props: ["lat", "lng"],
+  async mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      let googleMap = new google.maps.Map(
+        document.getElementById("googleMap"),
+        {
+          center: { lat: 43.616383, lng: -116.20276 },
+          zoom: 15
+        }
+      );
+    }
+  }
+};
+</script>
+<style scoped>
+#map {
+  height: 100%;
+}
+</style>
