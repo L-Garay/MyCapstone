651713ced7c6b9b799f551fa51c06da163852cb1
EricRooney <erooneous@gmail.com>
2020-01-22 10:42:03 -0700

    ActiveAddMap

--- client/src/views/Active.vue
+++ client/src/views/Active.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
+  <div class="container-fluid">
     <navbar />
-    <h3>{{activeOuting.name}}</h3>
+    <div class="row">
+      <div class="col-12">
+        <h3>{{activeOuting.name}}</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <GoogleMap />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/NavBar.vue";
+import GoogleMap from "@/components/GoogleMap.vue";
 export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    activeOuting();
+  },
   components: {
-    Navbar
+    Navbar,
+    GoogleMap
   },
   computed: {
     activeOuting() {
