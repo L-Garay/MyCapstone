91d0317d3b07567f7ae8c8a69234de2de89f19e6
Logan Garay <garay.logan@gmail.com>
2020-02-04 16:52:52 -0700

    production attempt3

---
+++ client/dist/precache-manifest.6b73a4efb66740e9e11b50cf4991c516.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56816a9af6fc2dbaa0d6",
+    "url": "/css/app.3246fd12.css"
+  },
+  {
+    "revision": "2b49b4adc2ee7a179425c7ae2624056d",
+    "url": "/img/3drinks.2b49b4ad.jpg"
+  },
+  {
+    "revision": "5d9047e981e6978184e77477d8f568ae",
+    "url": "/img/BarHopLogo.5d9047e9.png"
+  },
+  {
+    "revision": "9336537c536ab4024193a136d2cc302f",
+    "url": "/img/bar1.9336537c.jpg"
+  },
+  {
+    "revision": "31177f64a2028c47b05f925bcb58017c",
+    "url": "/img/bar2.31177f64.jpg"
+  },
+  {
+    "revision": "5ca29136e1c8d139d1e0ddbe0f79ac0c",
+    "url": "/img/bar3.5ca29136.jpg"
+  },
+  {
+    "revision": "54df7418c7b9f257e1b8cae53d6493cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56816a9af6fc2dbaa0d6",
+    "url": "/js/app.85072057.js"
+  },
+  {
+    "revision": "01d0880b406915cca3a9",
+    "url": "/js/chunk-vendors.cf4e59fb.js"
+  },
+  {
+    "revision": "a61432c095a170bd1e18a8b5d5825761",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
